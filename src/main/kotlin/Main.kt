5fed85986b82dfe48634f86b1f19f9de7e4fd809
Alexander Mandrikov <mandrikov@pm.me>
2023-06-01 17:05:40 +0700

    add commented dev code to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,22 @@
 package com.example.anidiff_jvm
 
+import com.example.anidiff_jvm.fetchers.MalFetcher
 import com.example.anidiff_jvm.settings.Settings
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
 
 fun main(args: Array<String>) {
     Settings.init()
     println(Settings)
+
+//    runBlocking {
+//        val manga = MalFetcher.mangaList()
+//        val content = Json.encodeToString(manga)
+//        println(manga)
+//
+//        File("mal_json.txt").writeText(content)
+//    }
+
 }
