d582a6ac56fde47b39c7b5b0edba2b9a1dd0ae1b
Alexander Mandrikov <mandrikov@pm.me>
2023-06-01 12:12:57 +0700

    add Settings

---
+++ src/main/kotlin/settings/Settings.kt
@@ -0,0 +1,39 @@
+package com.example.anidiff_jvm.settings
+
+object Settings {
+    private const val shikiPrefix = "https://shikimori.me"
+    private const val malPrefix = "https://myanimelist.net"
+
+    private lateinit var shikiName: String
+    private lateinit var malName: String
+
+    fun init() {
+        println("Enter Shikimori Name:")
+        shikiName = readln()
+
+        println("Enter MyAnimelist Name:")
+        malName = readln()
+    }
+
+    fun shikiMangaUrl(): String {
+        return "$shikiPrefix/$shikiName/list_export/mangas.json"
+    }
+
+    fun shikiAnimeUrl(): String {
+        return "$shikiPrefix/$shikiName/list_export/animes.json"
+    }
+
+    fun malMangaUrl(offset: Int): String {
+        return "$malPrefix/mangalist/$malName/load.json?offset=${offset}"
+    }
+
+    fun malAnimeUrl(offset: Int): String {
+        return "$malPrefix/animelist/$malName/load.json?offset=${offset}"
+    }
+
+    override fun toString(): String {
+        return """
+            Settings(shikiPrefix="$shikiPrefix", shikiName="$shikiName", malPrefix="$malPrefix", malName="$malName")
+        """.trimIndent()
+    }
+}
