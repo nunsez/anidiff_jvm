a12dab33306d0ddb7bf1f07a92cb51a26c754f1b
Alexander Mandrikov <mandrikov@pm.me>
2023-06-02 12:49:40 +0700

    update Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,20 @@
 package com.example.anidiff_jvm
 
+import com.example.anidiff_jvm.entities.Entity
+import com.example.anidiff_jvm.entities.EntityStatus
+import com.example.anidiff_jvm.entities.MalMangaEntity
+import com.example.anidiff_jvm.entities.ShikiMangaEntity
 import com.example.anidiff_jvm.fetchers.Fetcher
 import com.example.anidiff_jvm.fetchers.MalFetcher
 import com.example.anidiff_jvm.fetchers.ShikiFetcher
 import com.example.anidiff_jvm.settings.Settings
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 
 fun main(args: Array<String>) {
-    Settings.init()
-    println(Settings)
-
-//    runBlocking {
-//        val manga = MalFetcher.mangaList()
-//        val content = Json.encodeToString(manga)
-//        println(manga)
-//
-//        File("mal_json.txt").writeText(content)
-//    }
-
+//    Settings.init()
+//    println(Settings)
 }
