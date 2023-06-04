28c45dab4bd7a1ad33b40b963867be17488bf36e
Alexander Mandrikov <mandrikov@pm.me>
2023-06-04 12:19:22 +0700

    make main fun suspend

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,30 +6,27 @@ import com.example.anidiff_jvm.entities.MangaEntity
 import com.example.anidiff_jvm.fetchers.MalFetcher
 import com.example.anidiff_jvm.fetchers.ShikiFetcher
 import com.example.anidiff_jvm.settings.Settings
-import kotlinx.coroutines.runBlocking
 
-fun main() {
+suspend fun main() {
     Settings.init()
 
     val malFetcher = MalFetcher()
     val shikiFetcher = ShikiFetcher()
 
-    runBlocking {
-        val malAnime = malFetcher.animeList()
-        val shikiAnime = shikiFetcher.animeList()
+    val malAnime = malFetcher.animeList()
+    val shikiAnime = shikiFetcher.animeList()
 
-        val malManga = malFetcher.mangaList()
-        val shikiManga = shikiFetcher.mangaList()
+    val malManga = malFetcher.mangaList()
+    val shikiManga = shikiFetcher.mangaList()
 
-        val animeDiff: List<AnimeEntity> = compareEntityLists(malAnime, shikiAnime)
-        val mangaDiff: List<MangaEntity> = compareEntityLists(malManga, shikiManga)
+    val animeDiff: List<AnimeEntity> = compareEntityLists(malAnime, shikiAnime)
+    val mangaDiff: List<MangaEntity> = compareEntityLists(malManga, shikiManga)
 
-        println("Anime Diff:")
-        animeDiff.forEach(::println)
+    println("Anime Diff:")
+    animeDiff.forEach(::println)
 
-        println("Manga Diff:")
-        mangaDiff.forEach(::println)
-    }
+    println("Manga Diff:")
+    mangaDiff.forEach(::println)
 
     println("Press enter to exit")
     readln()
